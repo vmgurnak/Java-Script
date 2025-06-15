027ad9141e8e350611835721b6dc14d66c819fb6
Valeriy Gurnak <vmgurnak@gmail.com>
2025-06-15 12:07:34 +0300

    js-hw-07

---
+++ js/01-gallery.js
@@ -0,0 +1,130 @@
+// Задание 1 - галерея изображений
+
+// Создай галерею с возможностью клика по её элементам и просмотра полноразмерного изображения в модальном окне. Посмотри демо видео работы галереи.
+
+// Выполняй это задание в файлах 01-gallery.html и 01-gallery.js. Разбей его на несколько подзадач:
+
+// 1. Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+// 2. Реализация делегирования на ul.gallery и получение url большого изображения.
+// 3. Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
+// 4. Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
+// 5. Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
+
+// Разметка элемента галереи
+
+// Ссылка на оригинальное изображение должна храниться в data-атрибуте source на элементе <img>, и указываться в href ссылки. Не добавляй другие HTML теги или CSS классы кроме тех, что есть в этом шаблоне.
+
+//  <li class="gallery__item">
+//    <a class="gallery__link" href="large-image.jpg">
+//      <img
+//        class="gallery__image"
+//        src="small-image.jpg"
+//        data-source="large-image.jpg"
+//        alt="Image description"
+//      />
+//    </a>
+//  </li>;
+
+// Обрати внимание на то, что изображение обернуто в ссылку, а значит при клике по умолчанию пользователь будет перенаправлен на другую страницу. Запрети это поведение по умолчанию.
+
+// Закрытие с клавиатуры
+// Этот функционал не обязателен при сдаче задания, но будет хорошей дополнительной практикой.
+// Добавь закрытие модального окна по нажатию клавиши Escape. Сделай так, чтобы прослушивание клавиатуры было только пока открыто модальное окно. У библиотеки basicLightbox есть метод для программного закрытия модального окна.
+
+// Импорт массива объектов galleryItems
+import { galleryItems } from './gallery-items.js';
+
+// 1. Создание и рендер разметки по массиву данных galleryItems и шаблону элемента галереи.
+
+// Поиск ul.list
+const container = document.querySelector('.gallery');
+// console.log(container);
+
+// Функция для создание разметки, метод map преобразование массива galleryItems, создание нового массива с элементом строки, деструктуризация объекта, метод join преобразование массива в строку
+
+function createMarkup(arr) {
+  // функция возвращает строку с разметкой
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join('');
+}
+
+// Добавление строки разметки в DOM дерево, метод insertAdjacentHTML, "beforebegin" - внутри elem, после всех детей
+// Вызов функции reateMarkup(arr) с параметром galleryItems
+
+container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+// 2. Реализация делегирования на ul.gallery и получение url большого изображения.
+// 3. Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используется CDN сервис jsdelivr, добавлены в проект ссылки на минифицированные (.min) файлы библиотеки.
+// console.log(basicLightbox);
+
+// Слушатель по click на ul.gallery, делегирование клика
+container.addEventListener('click', handlerImageClick);
+
+// Коллбєк-функция для клика
+function handlerImageClick(evt) {
+  // Сброс стандартных действий при клике
+  evt.preventDefault();
+
+  // Проверка от обратного, где делается клик по названию класса gallery__image на теге  img
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  // Получение data атрибута(ссылка на оригинальное изображение) при клике
+  // Деструктуризация data-source
+  const { source } = evt.target.dataset;
+  // console.log(evt.target.dataset);
+  // console.log(source);
+
+  // Получение выбранного объекта при клике, метод Find поиск по атрбуту data-source
+  const galleryItem = galleryItems.find(({ original }) => original === source);
+  // console.log(galleryItem);
+
+  // Подключение модального окна с разметкой, библиотека basicLightbox
+  const instance = basicLightbox.create(`
+          <img src="${galleryItem.original}" alt="${galleryItem.description}" width="1280">
+  `);
+
+  instance.show();
+
+  // Закрытие модального окна клавишей Esc
+  // Слушатель события нажатия клавиши
+  document.addEventListener('keydown', handlerEsc);
+
+  // Коллбэк-функция при нажатии клавиши с проверкой Esc, закрытие модального окна
+  function handlerEsc(evt) {
+    console.log(evt.code);
+    if (evt.code === 'Escape') {
+      console.log('Нажата клавиша Esc');
+      //  Закрытие модального окна
+      instance.close();
+      // Снятие слушателя c клавиатуры
+      document.removeEventListener('keydown', handlerEsc);
+    }
+  }
+  // console.log(instance);
+}
+
+// Второй вариант с свойствами onShow:, onClose:
+// const instance = basicLightbox.create(`
+//      <div class="modal">
+//         <img src="${galleryItem.original}" alt="${galleryItem.description}" width="1280">
+//      </div>
+//   `,  {
+//       onShow: () => document.addEventListener('keydown', handlerEsc),
+//       onClose: () => document.removeEventListener('keydown', handlerEsc)
+//     }
+//   instance.show()
---
+++ js/02-lightbox.js
@@ -0,0 +1,68 @@
+// Задание 2 - библиотека SimpleLightbox
+
+// Сделай такую же галерею как в первом задании, но используя библиотеку SimpleLightbox, которая возьмет на себя обработку кликов по изображениям, открытие и закрытие модального окна, а также пролистывание изображений при помощи клавиатуры. Посмотри демо видео работы галереи с подключенной библиотекой.
+
+// Необходимо немного изменить разметку карточки галереи, используй этот шаблон.
+
+{
+  /* <li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+  </a>
+</li>; */
+}
+
+// Выполняй это задание в файлах 02-lightbox.html и 02-lightbox.js. Разбей его на несколько подзадач:
+
+// 1. Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи. Используй готовый код из первого задания.
+// 2. Подключение скрипта и стилей библиотеки используя CDN сервис cdnjs. Необходимо добавить ссылки на два файла: simple-lightbox.min.js и simple-lightbox.min.css.
+// 3. Инициализация библиотеки после того как элементы галереи созданы и добавлены в ul.gallery. Для этого ознакомься с документацией SimpleLightbox - в первую очередь секции «Usage» и «Markup».
+// 4. Посмотри в документации секцию «Options» и добавь отображение подписей к изображениям из атрибута alt. Пусть подпись будет снизу и появляется через 250 миллисекунд после открытия изображения.
+
+// Импорт массива объектов
+import { galleryItems } from './gallery-items.js';
+// console.log(galleryItems);
+
+// 1. Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи. Используй готовый код из первого задания.
+
+// Поиск ul.list
+const container = document.querySelector('.gallery');
+
+// Функция createMarkup(arr) для создание разметки, метод map, преобразование массива galleryItems, создание нового массива с элементом строки, деструктуризация объекта, метод join преобразование массива в строку
+
+function createMarkup(arr) {
+  // функция возвращает строку с разметкой
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join('');
+}
+
+// Добавление строки разметки в DOM дерево, метод insertAdjacentHTML, "beforebegin" - внутри elem, после всех детей
+// Вызов функции reateMarkup(arr) с параметром массив объектов galleryItems
+
+container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+// 2.Подключение скрипта и стилей библиотеки используя CDN сервис cdnjs. Необходимо добавить ссылки на два файла: simple-lightbox.min.js и simple-lightbox.min.css.
+
+// 3. Инициализация библиотеки после того как элементы галереи созданы и добавлены в ul.gallery. Для этого ознакомься с документацией SimpleLightbox - в первую очередь секции «Usage» и «Markup».
+
+// Подключение библиотеки
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  // Добавление подписи из атрибута 'alt' и задержки появления подписи
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+// console.log(lightbox);
