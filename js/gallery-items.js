253f97af8bd5caabc20cae017ae73e4a30de99ca
Valeriy Gurnak <vmgurnak@gmail.com>
2025-06-16 11:25:33 +0300

    js-hw-07

--- js/gallery-items.js
+++ js/gallery-items.js
@@ -1,6 +1,6 @@
-export { galleryItems };
+// export { galleryItems };
 
-const galleryItems = [
+export const galleryItems = [
   {
     preview:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
